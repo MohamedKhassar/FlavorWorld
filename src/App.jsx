c8b08582e8a923605b1ca7028a8f1282355bec11
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-26 15:59:48 +0100

    Update Home component with new design and add AOS animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter , Routes , Route } from "react-router-dom"
 import "./App.css"
-import Home from "./components/Home"
+import Home from "./Components/Home"
 export default function App() {
   return (
     <BrowserRouter>
