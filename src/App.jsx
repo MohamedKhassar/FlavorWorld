42908e89759d9aa42db06db6556fb19d2829789f
soka1998 <soukainaharifi18@gmail.com>
2023-12-29 15:04:55 +0100

    add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Home from "./Components/Home"
 import "./App.css"
+
 export default function App() {
   return (
-    <h1 className="text-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="*" element ={<h1>page not Found</h1>}/>
+      </Routes>
+   </BrowserRouter>
   )
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      soukaaaa
+    </div>
+  )
+}
+
+export default Home
