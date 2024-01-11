61118df1e14ccdf0277e33b40804f674444bbd6b
khassarMohamed <mohamedkhassar775@gmail.com>
2024-01-11 16:50:59 +0100

    edit

--- src/pages/Plate.jsx
+++ src/pages/Plate.jsx
@@ -1,5 +1,7 @@
 // import axios from "axios";
 // import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 const Plate = () => {
     const id = useParams().id
